cb95a9998899f72d62fbfca9175037d448d782a8
TimosPal <timospale@gmail.com>
2021-01-05 04:24:33 +0200

    Removed old pages , finished form validation

--- js/appointment.js
+++ js/appointment.js
@@ -2,9 +2,14 @@ const form = document.getElementById("contact-form");
 const name_lastName = document.getElementById("name");
 const email = document.getElementById("email");
 const date = document.getElementById("date");
-const time = document.getElementById("time");
+const timeAp = document.getElementById("time");
 const subject = document.getElementById("subject");
 const phone = document.getElementById("phone");
+
+
+setInputFilter(phone, function(value) {
+    return /^\d*\.?\d*$/.test(value); // Allow digits and '.' only, using a RegExp
+  });
 
 form.addEventListener("submit", (e)=> {
     e.preventDefault();
@@ -12,11 +17,26 @@ form.addEventListener("submit", (e)=> {
     checkInputs();
 });
 
+function setMinDateToToday(){
+    var today = new Date();
+    var dd = today.getDate();
+    var mm = today.getMonth()+1; //January is 0!
+    var yyyy = today.getFullYear();
+    if(dd<10){
+            dd='0'+dd
+    }if(mm<10){
+            mm='0'+mm
+    } 
+    
+    today = yyyy+'-'+mm+'-'+dd;
+    date.setAttribute("min", today);
+}
+
 function checkInputs(){
     const name_lastNameValue = name_lastName.value.trim();
     const emailValue = email.value.trim();
     const dateValue = date.value.trim();
-    const timeValue = time.value.trim();
+    const timeValue = timeAp.value.trim();
     const subjectValue = subject.value.trim();
     const phoneValue = phone.value.trim();
 
@@ -37,6 +57,50 @@ function checkInputs(){
     } else{
         setSuccessFor(subject);
     }
+
+    if(dateValue === ''){
+        setErrorFor(date, "Συμπληρώστε την ημέρα που επιθυμείτε");
+    } else{
+        setSuccessFor(date);
+    }
+
+    var day = new Date(dateValue).getUTCDay();
+    if([6,0].includes(day)){
+        setErrorFor(date, "Συμπληρώστε μιαμερα εντός των ημερών λειτουργίας");
+    }else{
+        setSuccessFor(date);
+    }
+
+    if(timeValue === ''){
+        setErrorFor(timeAp, "Συμπληρώστε την ώρα που επιθυμείτε");
+    } else{
+        setSuccessFor(timeAp);
+    }
+ 
+    if(timeValue !== ''){
+        // get input time
+        var time = timeValue.split(":");
+        
+        var hour = parseInt(time[0]);
+        console.log(hour);
+        var min = parseInt(time[1]);
+        
+        console.log(time);
+        var timeInMin = 60*hour + min;
+
+        if (timeInMin > 1200 || timeInMin < 480) {
+            setErrorFor(timeAp, "Συμπληρώστε μια ώρα εντός του ωράριου λειτουργίας");
+        } else {
+            setSuccessFor(timeAp);
+        }
+    }
+    
+    if(phoneValue === ''){
+        setErrorFor(phone, "Συμπληρώστε το τηλεφωνό σας");
+    } else{
+        setSuccessFor(phone);
+    }
+
 }
 
 function setErrorFor(input, message){
@@ -53,3 +117,21 @@ function setSuccessFor(input){
 
     formControl.className = "form-group success";
 }
+
+// Restricts input for the given textbox to the given inputFilter function.
+function setInputFilter(textbox, inputFilter) {
+    ["input", "keydown", "keyup", "mousedown", "mouseup", "select", "contextmenu", "drop"].forEach(function(event) {
+      textbox.addEventListener(event, function() {
+        if (inputFilter(this.value)) {
+          this.oldValue = this.value;
+          this.oldSelectionStart = this.selectionStart;
+          this.oldSelectionEnd = this.selectionEnd;
+        } else if (this.hasOwnProperty("oldValue")) {
+          this.value = this.oldValue;
+          this.setSelectionRange(this.oldSelectionStart, this.oldSelectionEnd);
+        } else {
+          this.value = "";
+        }
+      });
+    });
+  }
